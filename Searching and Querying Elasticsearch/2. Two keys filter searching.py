d60b935e1230186fbb2cd95baafdc8c56ff4bbaf
Yash Janeshwar <yash8135512@gmail.com>
2024-02-19 14:05:24 +0530

    Two keys filter search in an index

---
+++ Searching and Querying Elasticsearch/2. Two keys filter searching.py
@@ -0,0 +1,46 @@
+from elasticsearch import Elasticsearch
+from pprint import pprint
+
+ELASTIC_PASSWORD = "elastic"
+ELASTIC_USERNAME = "elastic"
+ELASTIC_PATH = "http://localhost:9200"
+INDEX_NAME = "nba_players"
+
+
+def connect_to_elastic() -> Elasticsearch:
+    client = Elasticsearch(
+        ELASTIC_PATH,
+        verify_certs=False,
+        basic_auth=(ELASTIC_USERNAME, ELASTIC_PASSWORD)
+    )
+    return client
+
+
+def main():
+    es = connect_to_elastic()
+    # create a query dict that matches documents where the 'team' field equals 'Nets' and the 'first_name' field equals 'James'
+    # kibana search syntax -> team: "Nets" and first_name: "James"
+    query = {
+        "query": {
+            "bool": {
+                "must": [
+                    {"match": {"team": "Nets"}},
+                    {"match": {"first_name": "James"}}
+                ]
+            }
+        }
+    }
+    # execute the search and print the results
+    response = es.search(index=INDEX_NAME, body=query)
+
+    # see full response from the server
+    print("response:")
+    pprint(response)
+
+    # get only the documents
+    for hit in response['hits']['hits']:
+        print(hit['_source'])
+
+
+if __name__ == '__main__':
+    main()
