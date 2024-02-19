d950fa956a40183dfc5380cf0c0158aeca546986
Yash Janeshwar <yash8135512@gmail.com>
2024-02-19 13:55:35 +0530

    Create indices and its mapping(schema)

---
+++ Indexing and Managing Documents with python/1. Creating indices.py
@@ -0,0 +1,60 @@
+from elasticsearch import Elasticsearch
+
+ELASTIC_PASSWORD = "elastic"
+ELASTIC_USERNAME = "elastic"
+ELASTIC_PATH = "http://localhost:9200"
+INDEX_NAME = "nba_players"
+
+mapping = {
+    "first_name": {
+        "type": "text"
+    },
+    "last_name": {
+        "type": "text"
+    },
+    "date_of_birth": {
+        "type": "date"
+    },
+    "position": {
+        "type": "keyword"
+    },
+    "team": {
+        "type": "keyword"
+    },
+    "avg_scoring": {
+        "type": "float"
+    },
+    "avg_rebound": {
+        "type": "float"
+    },
+    "avg_assist": {
+        "type": "float"
+    },
+    "country": {
+        "type": "keyword"
+    }}
+
+
+def connect_to_elastic() -> Elasticsearch:
+    client = Elasticsearch(
+        ELASTIC_PATH,
+        verify_certs=False,
+        basic_auth=(ELASTIC_USERNAME, ELASTIC_PASSWORD)
+    )
+
+    return client
+
+ 
+def main():
+    client = connect_to_elastic()
+    print(client.info())
+    client.indices.create(index=INDEX_NAME,
+                        body={
+                            'mappings': {
+                                'properties': mapping
+                            }
+                        })
+
+
+if __name__ == '__main__': 
+    main()
