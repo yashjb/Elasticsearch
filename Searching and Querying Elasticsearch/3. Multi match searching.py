2ade63a0fdc0829a84d490e9d426de461ac3be27
Yash Janeshwar <yash8135512@gmail.com>
2024-02-19 14:07:17 +0530

    Multi match search query in an index by adding multiple fields to search for.

---
+++ Searching and Querying Elasticsearch/3. Multi match searching.py
@@ -0,0 +1,43 @@
+from elasticsearch import Elasticsearch
+from pprint import pprint
+
+ELASTIC_PASSWORD = "elastic"
+ELASTIC_USERNAME = "elastic"
+ELASTIC_PATH = "http://localhost:9200"
+INDEX_NAME = "nba_players"
+
+
+def connect_to_elastic() -> Elasticsearch:
+    client = Elasticsearch(
+        ELASTIC_PATH,
+        verify_certs=False,
+        basic_auth=(ELASTIC_USERNAME, ELASTIC_PASSWORD)
+    )
+    return client
+
+
+def main():
+    es = connect_to_elastic()
+    # kibana search syntax -> first_name: "James" or last_name: "James"
+    query = {
+        "query": {
+            # "multi_match": {
+            #     "query": "Lakers",
+            #     "fields": ["full_name", "team"]
+            # },
+            "multi_match": {
+                "query": "James",
+                "fields": ["first_name", "last_name"]
+            }
+        }
+    }
+    # execute the search and print the results
+    response = es.search(index=INDEX_NAME, body=query)
+
+    # get only the documents
+    for hit in response['hits']['hits']:
+        print(hit['_source'])
+
+
+if __name__ == '__main__':
+    main()
